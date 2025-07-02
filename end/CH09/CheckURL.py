41d9a962e0bef4df95918009011aee65f4445e80
vg-grc <schoolmail389@sudomail.com>
2025-07-02 12:00:56 -0700

    Revert "Updated APIs"
    This reverts commit 4f4063af221c941e23127c064699fba946470b27.

--- end/CH09/CheckURL.py
+++ end/CH09/CheckURL.py
@@ -2,20 +2,17 @@
 # Script that checks URLs against Google's Safe Browsing API
 # https://developers.google.com/safe-browsing/v4
 # By Ed Goad
-# date: 2/5
+# date: 2/5/2021
 
 # Import Python modules
 import requests
+import json
 import configparser
-import os
 
 def get_api_key(key_name):
-    # Get the location of the secrets file
-    home_dir = os.path.expanduser( "~" )
-    secrets_file = os.path.join(home_dir, "secrets.ini" )
     # Create the ConfigParser and load the file
     config = configparser.ConfigParser()
-    config.read(secrets_file)
+    config.read("/home/pi/secrets.ini")
     # Get the API key and return
     api_key = config["APIKeys"][key_name]
     return api_key
@@ -54,21 +51,14 @@ def check_safebrowsing_url(token, threat, platform, test_url):
 token = get_api_key("GoogleSafeBrowsing")
 
 # Prompt user for platformTypes, Default to "LINUX"
-platform = input("What platform are you scanning form [LINUX] ") or "LINUX"
+platform = input("What platform are you scanning form [LINUX] ") \
+    or "LINUX"
 
 # Default threatTypes to "MALWARE"
 threat = input("What threat type [MALWARE] ") or "MALWARE"
 
 # Prompt user for threatEntries
-url = input("What URL to scan? ") or "http://testsafebrowsing.appspot.com/s/malware.html"
+url = input("What URL to scan? ") or \
+    "http://testsafebrowsing.appspot.com/s/malware.html"
 
-scan_results = check_safebrowsing_url(token, threat, platform, url)
-if "matches" in scan_results:
-    matches = scan_results["matches"]
-    for match in matches:
-        print(match["platformType"] + 
-              " / " + match["threatType"] + 
-              ": " + match["threat"]["url"]
-              )
-else:
-    print("No issues found")
+print(check_safebrowsing_url(token, threat, platform, url))
