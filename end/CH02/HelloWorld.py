8eb1ea3bbe0755b2009c7390e2e86032b57060f7
vg-grc <vgraham@greenriver.edu>
2025-07-20 08:43:09 -0700

    Christy_Willingham_1 hello world.py

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created by Ed Goad, 2/3
-print("Hello World")
+
+# Get the user name
+user_name = input("What is your name? ")
+# Print Hello + the user name
+print("Hello " + user_name)
+print("Hello", user_name)
+print(f"Hello {user_name}")
+message = "Hello " + user_name
+print( message )
