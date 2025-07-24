9e060679fcefa80fb63ed3ce2e549f700e4f0cec
vg-grc <vgraham@greenriver.edu>
2025-07-24 07:44:18 -0700

    Christy Willingham number and divisor.py

---
+++ end/CH03/Christy Willingham number and divisor.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# example number and divisor
+# By Christy Willingham
+
+number = int(input("Enter a number : "))
+divisor = int(input("Enter a divisor : "))
+
+if number % divisor == 0:
+    print ("Number is divisible")
+else:
+    print ("number is not divisible")
