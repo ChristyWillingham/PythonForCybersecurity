12ec47b6775003554757b84fb4095874b4c37463
vg-grc <vgraham@greenriver.edu>
2025-07-30 10:32:04 -0700

    Christy Willingham createhash1.py

--- end/CH06/CreateHash1.py
+++ end/CH06/CreateHash1.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 # Script that hashes a password
-# By Ed Goad
-# date: 11/1/2024
+# By Christy Willingham
+# date: 7/30/2025
 
 # Import Python modules
-from passlib.hash import sha256_crypt
-from passlib.hash import sha512_crypt
-from passlib.hash import lmhash
-from passlib.hash import nthash
-from passlib.hash import md5_crypt
+import crypt
 
 #prompt user for plain-text password
-plain_password = input("What is the password? ")
+plain_pass = input("What is the password? ")
 
 # Print out the hashes
-print("MD5          : {0}".format(md5_crypt.hash(plain_password)))
-print("SHA-256      : {0}".format(sha256_crypt.hash(plain_password)))
-print("SHA-512      : {0}".format(sha512_crypt.hash(plain_password)))
-print("LMhash       : {0}".format(lmhash.hash(plain_password)))
-print("NThash       : {0}".format(nthash.hash(plain_password)))
+print("MD5           : {0}".format(crypt.crypt(plain_pass, "$1$")))
+print("Blowfish      : {0}".format(crypt.crypt(plain_pass, "$2a$")))
+print("eksblofish    : {0}".format(crypt.crypt(plain_pass, "$2y$")))
+print("SHA-256       : {0}".format(crypt.crypt(plain_pass, "$5$")))
+print("SHA-512       : {0}".format(crypt.crypt(plain_pass, "$6$")))
