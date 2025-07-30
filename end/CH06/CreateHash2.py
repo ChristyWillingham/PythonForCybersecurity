2e1a107d14776648fcd748231166d9b24bd784f1
vg-grc <vgraham@greenriver.edu>
2025-07-30 10:52:02 -0700

    Christy Willingham createhash2.py

--- end/CH06/CreateHash2.py
+++ end/CH06/CreateHash2.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 # Script that hashes a password with provided salt
-# By Ed Goad
-# date: 11/1/2024
+# By Christy Willingham
+# date: 7/30/25
 
 # Import Python modules
-from passlib.hash import sha256_crypt
-from passlib.hash import sha512_crypt
+import crypt
 
 # Prompt user for plain-text password
-plain_password = input("What is the password? ")
+plain_pass = input("What is the password? ")
 salt = input("What is the salt? ")
 
 # Print out hashes
-print("SHA-256      : {0}".format(
-    sha256_crypt.hash(plain_password, salt=salt))
-    )
-print("SHA-512      : {0}".format(
-    sha512_crypt.hash(plain_password, salt=salt))
-    )
-
+print("MD5           : {0}".format(  \
+    crypt.crypt(plain_pass, "$1$" + salt)))
+print("Blowfish      : {0}".format( \
+    crypt.crypt(plain_pass, "$2a$" + salt)))
+print("eksblofish    : {0}".format( \
+    crypt.crypt(plain_pass, "$2y$" + salt)))
+print("SHA-256       : {0}".format( \
+    crypt.crypt(plain_pass, "$5$" + salt)))
+print("SHA-512       : {0}".format( \
+    crypt.crypt(plain_pass, "$6$" + salt)))
